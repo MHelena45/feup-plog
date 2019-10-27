1a85bcc9bf27fa097e7102948d8e545d43c9202f
MHelena45 <44232337+MHelena45@users.noreply.github.com>
2019-10-27 23:58:30 +0000

    checks same forms in the square board

--- Project1/Controller/game.pl
+++ Project1/Controller/game.pl
@@ -1,6 +1,7 @@
 :- include('../Model/dataStructs.pl').
 :- include('../View/boardPrinter.pl').
 :- include('../View/userInteractions.pl').
+:- include('verifications.pl').
 
 start :- 
     board(Board), % Initialization of the board
@@ -74,107 +75,6 @@ searchRow(Piece, [Piece| Rest], [0| Rest]).
 searchRow(Piece, [P| Rest], [P| More]) :-
     searchRow(Piece, Rest, More).
 
-% --------------------------- VERIFICATIONS -----------------------------
-% Check if row and Column are valid (between 1 and 4 inclusive)
-checkPosition(Pos) :-
-    Pos < 5,
-    Pos > 0. 
-
-% Accepted pieces
-checkPiece(cone).
-checkPiece(sphere).
-checkPiece(cylinder).
-checkPiece(cube).
-
-checkPlay(Piece, Row, Column, Player) :-
-    checkValidRow(Piece, Player, Row).
-
-% =================================
-%           Get Piece
-% =================================
-
-getPieceFromList([H|_T], 1, Piece) :-
-    Piece = H.
-
-getPieceFromList([_H|T], Index, Piece) :-
-    Index > 1,
-    Index1 is Index - 1,
-    getPieceFromList(T, Index1, Piece).
-
-getPieceFromBoard([H|_T], 1, Column, Piece) :-
-    getPieceFromList(H, Column, Piece).
-
-getPieceFromBoard([_H|T], Row, Column, Piece) :-
-    Row > 1,
-    Row1 is Row - 1,
-    getPieceFromBoard(T, Row1, Column, Piece).
-
-/* Check if Row and column don't have a piece, if true the game continues */
-isEmpty(Board, WhitePieces, BrownPieces, Row, Column, Player) :-
-    getPieceFromBoard(Board, Row, Column, Piece),
-    Piece == 0.
-
-/* if is not empty, player as to play again. A error mensage is displayed. */
-isEmpty(Board, WhitePieces, BrownPieces, Row, Column, P) :-
-    notEmpty(Row, Column),
-    play(P, Board, WhitePieces, BrownPieces).
-
-/* Piece we can not find in the same row, column ou square */
-opost(11, 12).
-opost(51, 52).
-opost(71, 72).
-opost(91, 92).
-
-opost(12, 11).
-opost(52, 51).
-opost(72, 71).
-opost(92, 91).
-
-valid_move(_Player, Board, _WhitePieces, _BrownPieces, Row, Column, Piece) :-
-    Row1 is Row + 1,
-    check_Top(Board, Row1, Column, Piece),
-    Row2 is Row - 1,
-    check_Down(Board, Row2, Column, Piece), 
-    Column1 is Column + 1,
-    check_Left(Board, Row, Column1, Piece),
-    Column2 is Column - 1,
-    check_Rigth(Board, Row, Column2, Piece).
-
-valid_move(Player, Board, WhitePieces, BrownPieces, _Row, _Column, _Piece) :-
-    wrongMove,
-    play(Player, Board, WhitePieces, BrownPieces).
-
-check_Top(_Board, 5, _Column, _Piece).
-check_Top(Board, Row, Column, Piece) :-
-    opost(Opost , Piece),
-    getPieceFromBoard(Board, Row, Column, PieceThere),
-    PieceThere =\= Opost,
-    NewRow is Row+1,
-    check_Top(Board, NewRow, Column, Piece).
-
-check_Down(_Board, 0, _Column, _Piece).
-check_Down(Board, Row, Column, Piece) :-
-    opost(Opost , Piece),
-    getPieceFromBoard(Board, Row, Column, PieceThere),
-    PieceThere =\= Opost,
-    NewRow is Row-1,
-    check_Down(Board, NewRow, Column, Piece).
-
-check_Left(_Board, _Row, 5, _Piece).
-check_Left(Board, Row, Collumn, Piece) :-
-    opost(Opost , Piece),
-    getPieceFromBoard(Board, Row, Collumn, PieceThere),
-    PieceThere =\= Opost,
-    NewCollumn is Collumn + 1,
-    check_Left(Board, Row, NewCollumn, Piece).
-
-check_Rigth(_Board, _Row, 0, _Piece).
-check_Rigth(Board, Row, Column, Piece) :-
-    opost(Opost , Piece),
-    getPieceFromBoard(Board, Row, Column, PieceThere),
-    PieceThere =\= Opost,
-    NewColumn is Column - 1,
-    check_Rigth(Board, Row, NewColumn, Piece).
 
 % --------------------------- TRANSLATIONS -----------------------------
 % White pieces
