220ffb480a4d5eb468c317ca40a0804282b6d949
Pasgas99 <pasgas99@hotmail.com>
2019-11-10 23:08:23 +0000

    Minor aditions

--- Project1/controller/computer.pl
+++ Project1/controller/computer.pl
@@ -3,15 +3,23 @@
 
 choose_move(Board, White_Pieces, Brown_Pieces, Level, Move, Player) :-
     valid_moves(Board, White_Pieces, Brown_Pieces, Player, List_Of_Moves),
-    get_move(Level, List_Of_Moves, Move).
+    get_move(Level, List_Of_Moves, Player, Board, Move).
 
 valid_moves(Board, White_Pieces, Brown_Pieces, Player, List_Of_Moves) :-
     findall(Move, valid_move(Move, Player, Board, White_Pieces, Brown_Pieces), List_Of_Moves).
 
 % Level 1 - Selects a random move
-get_move(1, List_Of_Moves, Move) :-
+get_move(1, List_Of_Moves, _Player, _Board, Move) :-
     random_member(Move, List_Of_Moves).
 
 % Level 2 - Selects best move based on value 
-get_move(2, _List_Of_Moves, _Move).
+get_move(2, List_Of_Moves, Player, Board, Move) :-
+    calculate_values(List_Of_Moves, Player, Board, Result), % result = List of [Move|Value]
+    get_best_move(Result, Move).
+
+calculate_values(List_Of_Moves, Player, Board, Result) :-
+
+get_best_move(Result, Move).
+
+value(Board, Player, Value).
 
