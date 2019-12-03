1b73343ffd44b3bb2973eca58722ae5e6e45762e
Helena Ferreira <ferreiraleninha52@gmail.com>
2019-12-03 23:06:03 +0000

    resctriction not valid for all the cases

--- Project2/game.pl
+++ Project2/game.pl
@@ -6,11 +6,16 @@ play(Row, RowValue, Column, ColumnValue) :-
     domain(Vars, 1, 9),  
     global_cardinality(Vars, [1-2, 2-2, 3-2, 4-2, 5-2, 6-2, 7-2, 8-2, 9-2]),
     checkPosition(Vars), 
+    % check if the indicated row have the rigth spacing between the 2 black squares
     Index1 is (Row - 1) * 2 + 1,
     Index2 is Row * 2,
     element(Index1, Vars, Element1), 
     element(Index2, Vars, Element2),
     Element2 #= Element1 + RowValue + 1, 
+    % check if the indicated row have the rigth spacing between the 2 black squares
+    element(Position1, Vars, Column), 
+    element(Position2, Vars, Column), 
+    Position2 #= Position1 + (ColumnValue * 2) + 2, % both squares are the first ou second in that row
     labeling([],Vars),
     write(Vars).
 
