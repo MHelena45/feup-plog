687e9122ac213a151d3f6aac2627c81522408c89
MHelena45 <ferreiraleninha52@gmail.com>
2020-02-13 16:54:11 +0000

    improve nxn disjoint

--- Project2/nxndisjoint.pl
+++ Project2/nxndisjoint.pl
@@ -3,50 +3,47 @@
 
 disjoint2_nxn(Board_Size, Column_Constraints, Row_Constraints, Options) :-   
     get_vars_matrix(Board_Size, 0, Vars),
-    check_distance_disjoint(Vars, 1, [], Rectangles, [], Margin ),
+    check_distance_disjoint(Vars, 1, [], Rectangles, [], Squares),
     disjoint2(Rectangles, [margin(a, a, 1, 1)]),
+    restrict_squares_per_column(Board_Size, Squares ),
     % constrain_columns(Vars, Board_Size, Column_Constraints, 1),
-    row_constraint(1, Row_Constraints, Vars),
+    % row_constraints(1, Row_Constraints, Vars),
     append_vars(Vars, [], Final_Vars),
     labeling(Options, Final_Vars),
     show_board_matrix(Vars).
 
-row_constraints([], _Vars).
-row_constraints([Row_Constraint | MoreConstraints], Vars) :-
-    row_constraint(1, Row_Constraint, Vars),
-    row_constraints(MoreConstraints, Vars).
+/**
+ * makes sure there are only two black squares per column
+ */
+restrict_squares_per_column( Board_Size, Squares ) :-
+    get_cardinality(Board_Size, [], List_Of_Cardinality),
+    global_cardinality(Squares, List_Of_Cardinality).   
 
-row_constraint(Row, [Row-Row_Value], [Line | _]) :-
-    element(S1, Line, 1), % first square of the first row
-    element(S2, Line, 1), % second square of the second row
-    disjoint2( 
-       [rect(S1, 1, Row, 1, Rectangle_Type), 
-        rect(S2, 1, Row, 1, Rectangle_Type) ],
-       [ margin(Rectangle_Type, Rectangle_Type, Row_Value, 0) ] ).
-
-
-row_constraint(Counter, Row_Constraint, [_Line1 | OtherLines]) :-
-   NextCounter is Counter + 1,
-   row_constraint(NextCounter, Row_Constraint, OtherLines).
+/**
+ * gets the list of cardinality being applied. Every integer number in the domain [1, board_size], 
+ * must appear twice.
+ */
+get_cardinality(0, List_Of_Cardinality, List_Of_Cardinality).
+get_cardinality(Board_Size, List_Of_Cardinality, Final_List_Of_Cardinality) :-
+    Board_Size1 is Board_Size  - 1,
+    append([Board_Size-2], List_Of_Cardinality, List_Of_Cardinality1),
+    get_cardinality(Board_Size1, List_Of_Cardinality1, Final_List_Of_Cardinality).
 
 /**
  * Disjoint tem a posição no vetor como x
  * usa as linhas a ser utilizadas como y
  */
-
-check_distance_disjoint( [  ], _Board_Size, Rectangles, Rectangles, Margin, Margin).
-check_distance_disjoint([Line1 | OtherLines], Counter, Rectangles, Final_Rectangles, Margin, Final_Margin ):-
+check_distance_disjoint( [  ], _Board_Size, Rectangles, Rectangles, Squares, Squares).
+check_distance_disjoint([Line1 | OtherLines], Counter, Rectangles, Final_Rectangles, Squares, Final_Squares ):-
 
     element(C1, Line1, 1), % first square of the first row
     element(C2, Line1, 1), % second square of the second row
-   
-    C1 #< C2,
-   
+    C1 #< C2,   
     NextCounter is Counter + 1,
     check_distance_disjoint( OtherLines, NextCounter,
     [   rect(C1, 1, Counter, 1, a), 
         rect(C2, 1, Counter, 1, a) | Rectangles], Final_Rectangles, 
-    [ margin(a, a, 1, 1) | Margin] , Final_Margin).
+    [ C1, C2| Squares] , Final_Squares).
 
 
 /**
